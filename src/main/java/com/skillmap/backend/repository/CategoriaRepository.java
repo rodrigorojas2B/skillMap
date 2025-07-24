ec41333ee1196afe70e342bf159756f5e5058499
Rodrigo <rodrigo.rojas.g@usach.cl>
2025-07-24 13:09:06 -0400

    limpieza de clases

--- src/main/java/com/skillmap/backend/repository/CategoriaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.skillmap.backend.repository;
-import com.skillmap.backend.model.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-    boolean existsByNombre(String nombre);
-}
--- src/main/java/com/skillmap/backend/repository/SkillRepository.java
+++
@@ -1,7 +0,0 @@
-package com.skillmap.backend.repository;
-import com.skillmap.backend.model.Categoria;
-import com.skillmap.backend.model.Skill;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface SkillRepository extends JpaRepository<Skill, Long> {
-    boolean existsByCategoria(Categoria categoria);
-}
